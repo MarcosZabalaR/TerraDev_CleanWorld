55ee6324f148735d8eac6cfab15607c0cc0b242b
pma152402 <pma152402@gmail.com>
2025-12-02 11:06:57 +0100

    feature/frontend/home.jsx setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
-import './App.css'
+import "./App.css";
+import WorldMap from "./assets/worldmap.webp";
 
 function App() {
-
   return (
     <>
-     <p className="font-bold border-2 text-amber-300">Mondongo</p>
+      <div className="bg-gradient-to-r from-blue-200 to-blue-100 h-[100vh] flex justify-center relative overflow-hidden">
+        <div className="text-white mt-20">
+          <span className="text-2xl font-medium drop-shadow-[1px_1px_0px_gray]">
+            Welcome to
+          </span>
+          <h1 className="text-9xl font-bold drop-shadow-[1px_3px_1px_gray]">
+            CleanWorld
+          </h1>
+          
+          <div className="flex justify-center mt-5">
+            <button className="border-3 border-neutral-400 px-3 py-2 bg-neutral-100 rounded-full mr-5 text-neutral-700 hover:scale-105 transition cursor-pointer">Sign In</button>
+            <button className="border-3 border-gray-400 px-3 py-2 bg-neutral-500 rounded-full hover:scale-105 transition cursor-pointer">Register</button>
+          </div>
+        </div>
+
+        <img
+          src={WorldMap}
+          className="h-220 w-220 spin absolute top-[50vh] z-10"
+        ></img>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
