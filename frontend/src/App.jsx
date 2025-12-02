70a34b5408d5a6a1fa063999d6369aeb566900cb
pma152402 <pma152402@gmail.com>
2025-12-02 13:15:40 +0100

    feature/frontend/Home.jsx COMPLETE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home.jsx'
+import Login from './components/Login.jsx'
 
 function App() {
-
   return (
     <>
-     <p className="font-bold border-2 text-amber-300">Mondongo</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<p>La página no existe</p>} />
+      </Routes>
+
+      <p className="font-bold border-2 text-amber-300">Mondongo</p>
     </>
   )
 }
