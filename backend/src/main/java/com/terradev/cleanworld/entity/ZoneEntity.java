312c380aa04bfb4b84012d6807da5321d93e295c
marcos <marcoszabalarodriguez@gmail.com>
2025-12-10 10:55:03 +0100

    docs: Added back & front documentation and backlog Sprint 1

--- backend/src/main/java/com/terradev/cleanworld/entity/ZoneEntity.java
+++ backend/src/main/java/com/terradev/cleanworld/entity/ZoneEntity.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Table(name = "zone")
 @Getter @Setter
 
-public class ZoneEntity {
+public class ZoneEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
