97fb0e15537e94b94ee9a15b007d8e5c563993eb
SrPirson <169342716+SrPirson@users.noreply.github.com>
2026-04-22 10:07:52 +0200

    feat: add analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react";
 import { I18nextProvider } from "react-i18next";
 import i18next from 'i18next';
 import global_es from "./locales/es/global.json";
@@ -41,6 +42,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <I18nextProvider i18n={i18next}>
       <App />
+      <Analytics />
     </I18nextProvider>
   </StrictMode>,
 )
